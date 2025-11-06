46879d2565be1da835aefc9f283af0effaad090a
Anhadh Virk <43526788+anhadh676842@users.noreply.github.com>
2025-11-06 17:31:55 +1000

    Added first implementation of module

---
+++ recognition/ImprovedUNet3D/module.py
@@ -0,0 +1,108 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# -----------------------------
+# Pre-activation Residual Block (Context Module)
+# -----------------------------
+class ResidualBlock3D(nn.Module):
+    def __init__(self, in_channels, out_channels, dropout=0.3, stride=1):
+        super().__init__()
+        self.stride = stride
+        self.conv1 = nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1, stride=stride)
+        self.norm1 = nn.InstanceNorm3d(out_channels)
+        self.act1 = nn.LeakyReLU(0.01)
+        
+        self.conv2 = nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.norm2 = nn.InstanceNorm3d(out_channels)
+        self.act2 = nn.LeakyReLU(0.01)
+        
+        self.dropout = nn.Dropout3d(dropout)
+        
+        if in_channels != out_channels or stride > 1:
+            self.skip = nn.Conv3d(in_channels, out_channels, kernel_size=1, stride=stride)
+        else:
+            self.skip = nn.Identity()
+    
+    def forward(self, x):
+        residual = self.skip(x)
+        x = self.conv1(x)
+        x = self.dropout(x)
+        x = self.norm2(x)
+        x = self.act2(x)
+        x = self.conv2(x)
+        return x + residual
+
+# -----------------------------
+# Localization Module
+# -----------------------------x
+class LocalizationModule(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv3x3 = nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.act = nn.LeakyReLU(0.01)
+        self.conv1x1 = nn.Conv3d(out_channels, out_channels // 2, kernel_size=1)
+    
+    def forward(self, x):
+        x = self.act(self.conv3x3(x))
+        x = self.conv1x1(x)
+        return x
+
+# -----------------------------
+# Upsample by voxel repetition
+# -----------------------------
+def upsample_repeat(x):
+    # Double each spatial dimension by repeating voxels
+    return x.repeat_interleave(2, dim=2).repeat_interleave(2, dim=3).repeat_interleave(2, dim=4)
+
+# -----------------------------
+# Full 3D U-Net with deep supervision
+# -----------------------------
+class ImprovedUNet3D(nn.Module):
+    def __init__(self, in_channels=1, out_channels=3, base_filters=16, dropout=0.3):
+        super().__init__()
+        self.conv1 = nn.Conv3d(in_channels, base_filters, kernel_size=3, padding=1)
+
+        # Encoder / context pathway
+        self.enc1 = ResidualBlock3D(base_filters, base_filters, dropout) 
+        self.enc2 = ResidualBlock3D(base_filters*2, base_filters*2, dropout, stride=2) 
+        self.enc3 = ResidualBlock3D(base_filters*4, base_filters*4, dropout, stride=2)
+        self.enc4 = ResidualBlock3D(base_filters*8, base_filters*8, dropout, stride=2)
+        self.enc5 = ResidualBlock3D(base_filters*16, base_filters*16, dropout, stride=2)
+        
+        # Decoder / localization pathway    
+        self.loc3 = LocalizationModule(base_filters*8 + base_filters*4, base_filters*4)
+        self.loc2 = LocalizationModule(base_filters*4 + base_filters*2, base_filters*2)
+        self.loc1 = LocalizationModule(base_filters*2 + base_filters, base_filters)
+        
+        # Deep supervision outputs
+        self.ds3 = nn.Conv3d(base_filters*4, out_channels, kernel_size=1)
+        self.ds2 = nn.Conv3d(base_filters*2, out_channels, kernel_size=1)
+        self.ds1 = nn.Conv3d(base_filters, out_channels, kernel_size=1)
+    
+    def forward(self, x):
+        # Encoder
+        e1 = self.enc1(x)
+        e2 = self.enc2(e1)
+        e3 = self.enc3(e2)
+        e4 = self.enc4(e3)
+        
+        # Decoder with upsample + concat + localization
+        u3 = upsample_repeat(e4)
+        u3 = torch.cat([u3, e3], dim=1)
+        l3 = self.loc3(u3)
+        
+        u2 = upsample_repeat(l3)
+        u2 = torch.cat([u2, e2], dim=1)
+        l2 = self.loc2(u2)
+        
+        u1 = upsample_repeat(l2)
+        u1 = torch.cat([u1, e1], dim=1)
+        l1 = self.loc1(u1)
+        
+        # Deep supervision: sum outputs from different levels
+        out = F.interpolate(self.ds3(l3), size=l1.shape[2:], mode='trilinear', align_corners=False)
+        out += F.interpolate(self.ds2(l2), size=l1.shape[2:], mode='trilinear', align_corners=False)
+        out += self.ds1(l1)
+        
+        return out
